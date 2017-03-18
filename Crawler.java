46283ba565ace7e27dcec986fead91ff531d7819
unknown <ahmad.w.kahla@gmail.com>
2017-03-19 00:29:09 +0200

    Crawler now fetches HTML docs only

--- Crawler.java
+++ Crawler.java
@@ -8,10 +8,11 @@ import java.io.PrintWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedList;
+
 import java.util.Set;
 import java.util.List;
 
@@ -35,7 +36,7 @@ public class Crawler {
 	}
 	public Crawler(String startLink, int maxPages){
 		
-		pagesToVisit = new LinkedList<String>();
+		pagesToVisit = fileToList("toVisit.txt");
 		visitedPages = fileToSet("visited.txt");
 		pagesToVisit.add(startLink);
 		this.maxPages = maxPages;
@@ -46,6 +47,20 @@ public class Crawler {
 			BufferedWriter bw = new BufferedWriter(fw);
 			PrintWriter out = new PrintWriter(bw)){
 				for(String link : set){
+					out.println(link);
+				}			    
+			}
+		catch (IOException e) {
+			    //exception handling left as an exercise for the reader
+				System.err.print(e.getMessage());
+			}		
+	}
+	public void listToFile(List<String> list,String filename){
+		
+		try(FileWriter fw = new FileWriter(filename, false);
+			BufferedWriter bw = new BufferedWriter(fw);
+			PrintWriter out = new PrintWriter(bw)){
+				for(String link : list){
 					out.println(link);
 				}			    
 			}
@@ -72,9 +87,28 @@ public class Crawler {
 		
 		return updatedPages;
 	}
+	
+	public List<String> fileToList(String filename){
+		List<String> updatedPages = new ArrayList<String>();
+		File toVisit = new File(filename);
+		if(!toVisit.exists()){
+			return updatedPages;
+		}			
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		    	updatedPages.add(line);
+		    }
+		}
+		catch (IOException e) {		    
+			System.err.print(e.getMessage());
+		}
+		
+		return updatedPages;
+	}
 	public static void main(String[] args) throws IOException{
 		Crawler myCrawler = new Crawler("https://en.wikipedia.org/wiki/Robots_exclusion_standard",100);
-		myCrawler.Crawl();
+		myCrawler.crawl();
 	}
 	public void createSiteDirectory(String siteDirectory){
 		File f = new File(siteDirectory);
@@ -83,7 +117,6 @@ public class Crawler {
 	}
 	public void createSiteHTML(String path, String Data) throws IOException{		
 		File f = new File(path);
-
 		f.getParentFile().mkdirs(); 
 		f.createNewFile();
 		FileWriter fw = new FileWriter(f, true);
@@ -101,19 +134,24 @@ public class Crawler {
 	    }
 	    return hostname;
 	}
-	public void Crawl(){
+	public void crawl(){
 		try {
 			while(!pagesToVisit.isEmpty() && visitedPages.size()<maxPages){
 				String nextPage = getNextPage();
-				if(nextPage == "")
+				if(nextPage == "")//to avoid empty strings
 					continue;
 				URL pageURL = new URL(nextPage);
-				
+				URLConnection c = pageURL.openConnection();
+				String contentType = c.getContentType();
+				//System.out.println(contentType);
+				if(!contentType.contains("text/html; charset=UTF-8"))//only HTML docs
+					continue;
 				/*Connection connection =Jsoup.connect(nextPage);
 				if(connection.response().statusCode() != 200)
 					continue;
 					*/
 				Document doc = Jsoup.connect(nextPage).get();
+				System.out.println("Now visiting: " + nextPage);
 				createSiteHTML(getHostName(nextPage)+pageURL.getPath()+".html", doc.toString());
 				Elements links = doc.getElementsByTag("a");
 				for (Element link : links) {
@@ -122,7 +160,8 @@ public class Crawler {
 						pagesToVisit.add(linkHref);
 					}				  
 				}
-				setToFile(visitedPages, "visited.txt");				
+				setToFile(visitedPages, "visited.txt");
+				listToFile(pagesToVisit, "toVisit.txt");
 			}		
 				
 		} catch (IOException e) {
